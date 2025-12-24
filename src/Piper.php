865ecab040aceebd07f3a60ad5024099fd73c5dc
Steven <burnett@posteo.de>
2025-12-24 21:52:33 +0000

    feat(piper): add phpstan+phpcs + refactor based on private constructor

--- src/Piper.php
+++ src/Piper.php
@@ -8,21 +8,26 @@ use Closure;
 final class Piper
 {
     private readonly Closure $fn;
+
+    /** @var array<mixed> */
     private array $args = [];
+
+    private function __construct(callable $fn)
+    {
+        $this->fn = Closure::fromCallable($fn);
+    }
 
     public static function with(callable $fn): self
     {
         return self::_($fn);
     }
-    
+
     public static function _(callable $fn): self
     {
-        $self = new self();
-        $self->fn = Closure::fromCallable($fn);
-        
-        return $self;
+        return new self($fn);
     }
-    
+
+    /** @param mixed ...$args */
     public function args(mixed ...$args): self
     {
         $this->args = [...$this->args, ...$args];
